abd1cae8710ae57262ca2f81f2751a72e89d5ae9
Chad Zuniga <czuniga10@Chads-MacBook-Pro.local>
2019-07-17 12:50:46 -0600

    user able to deploy ships and ships show up on map

--- battleship.java
+++ battleship.java
@@ -1,13 +1,15 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Main {
+public class battleship {
     private static int size = 10;
     private static int ships = 5;
     public static int[][] board = new int[size][size];
 
 
     public static void main(String[] args) {
+        showMap();
+        playerShips();
         showMap();
     }
 
@@ -19,6 +21,9 @@ public class Main {
                 if(board[row][col] == 0){
                     System.out.print(" ");
                 }
+                if(board[row][col] == 1){
+                    System.out.print("@");
+                }
 
             }
             System.out.println("|" + row);
@@ -26,6 +31,29 @@ public class Main {
         numberLine();
     }
 
+    public static void playerShips(){
+        for(int i = 1; i <= ships; i++) {
+            deployShips(i);
+        }
+    }
+
+    public static void deployShips(int i){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter X Coordinate for Ship " + i + ": ");
+        int x = input.nextInt();
+        System.out.print("Enter Y Coordinate for Ship " + i + ": ");
+        int y = input.nextInt();
+        if(x < 10 && y < 10 && board[x][y] == 0) {
+            board[x][y] = 1;
+        }else if(x > 10 || y > 10){
+            System.out.println("Invalid Coordinates... try again");
+            deployShips(i);
+        }else{
+            System.out.println("You already chose these coordinates... try again");
+            deployShips(i);
+        }
+
+    }
     public static void numberLine(){
         System.out.print("  ");
         for(int i = 0; i < size; i++){
@@ -33,5 +61,4 @@ public class Main {
         }
         System.out.println();
     }
-
 }
